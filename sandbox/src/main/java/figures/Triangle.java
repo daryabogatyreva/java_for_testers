f78c3ea64960e73a17fedb63dc08b1fa74479296
daryabogatyreva <dary.bogatyreva@gmail.com>
2024-09-16 04:35:49 +0300

    Добавлена проверка на отрицательные стороны и сумму сторон треугольника

--- sandbox/src/main/java/figures/Triangle.java
+++ sandbox/src/main/java/figures/Triangle.java
@@ -4,12 +4,20 @@ import static java.lang.Math.sqrt;
 
 public record Triangle(double a, double b, double c) {
 
+    public void check() {
+        if (a < 0 || b < 0 || c < 0) {
+            throw new AssertionError("Стороны треугольника должны быть положительными числами");
+        } else if (a + b < c || a + c < b || b + c < a) {
+            throw new AssertionError("Сумма сторон треугольника должна быть не меньше третьей стороны");
+        }
+    }
+
     public double perimeter() {
         return (this.a + this.b + this.c);
     }
 
     public double area() {
-        double p = perimeter()/2;
+        double p = perimeter() / 2;
         return (sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)));
     }
 }
--- sandbox/src/test/java/geometry/TriangleTests.java
+++ sandbox/src/test/java/geometry/TriangleTests.java
@@ -9,11 +9,17 @@ import static java.lang.Math.sqrt;
 public class TriangleTests {
 
     static double a = 5.0;
-    static double b = 5.0;
-    static double c = 5.0;
+    static double b = 8.0;
+    static double c = 8.0;
     static double p = (a + b + c) / 2;
     private static double expectedPerimeter = a + b + c;
     private static double expectedArea = sqrt(p * (p - a) * (p - b) * (p - c));
+
+    @Test
+    void checkNegativeTriangle() {
+        var triangle = new Triangle(a, b, c);
+        triangle.check();
+    }
 
     @Test
     void calculatePerimeter() {
@@ -29,4 +35,5 @@ public class TriangleTests {
         double actualArea = s.area();
         Assertions.assertEquals(expectedArea, actualArea);
     }
+
 }
