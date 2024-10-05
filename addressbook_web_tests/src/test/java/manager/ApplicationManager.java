3428f557975037c5112b00d9bc4845605752759f
daryabogatyreva <dary.bogatyreva@gmail.com>
2024-10-05 15:24:31 +0300

    Добавлен applicationManager и выполнено преобразование кода

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -1,34 +1,16 @@
-import org.junit.jupiter.api.BeforeEach;
+package manager;
+
+import model.GroupDate;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestBase {
-
+public class ApplicationManager {
     protected static WebDriver driver;
 
-    protected static void createGroup() {
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("group name");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("group header");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("group footer");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    protected static void deleteGroup() {
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.xpath("(//input[@name='delete'])[2]")).click();
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    @BeforeEach
-    public void setUp() {
+    public void init() {
         if (driver == null) {
             driver = new ChromeDriver();
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
@@ -41,7 +23,7 @@ public class TestBase {
         }
     }
 
-    protected boolean canElementPresent(By locator) {
+    public boolean canElementPresent(By locator) {
         try {
             driver.findElement(locator);
             return false;
@@ -50,7 +32,25 @@ public class TestBase {
         }
     }
 
-    protected void openGroupsPage() {
+    public void createGroup(GroupDate group) {
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys(group.name());
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys(group.header());
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(group.footer());
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    public void deleteGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.xpath("(//input[@name='delete'])[2]")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    public void openGroupsPage() {
         if (canElementPresent(By.name("new"))) {
             driver.findElement(By.linkText("groups")).click();
         }
