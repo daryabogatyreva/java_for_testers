0ee1564199b1ede264e240cfb39ac980b1a55fcb
daryabogatyreva <dary.bogatyreva@gmail.com>
2024-10-26 18:07:10 +0300

    оптимизированы импорты

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -4,7 +4,6 @@ import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
